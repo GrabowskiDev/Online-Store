75d01c39025f700c914eccb062a4e8b7b3ddf059
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2024-12-20 02:43:50 +0100

    Add Mantine ui library

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+import '@mantine/core/styles.css';
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+
 const geistSans = Geist({
 	variable: '--font-geist-sans',
 	subsets: ['latin'],
@@ -23,8 +26,13 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+		<html lang="en" {...mantineHtmlProps}>
+			<head>
+				<ColorSchemeScript />
+			</head>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<MantineProvider>{children}</MantineProvider>
+			</body>
 		</html>
 	);
 }
