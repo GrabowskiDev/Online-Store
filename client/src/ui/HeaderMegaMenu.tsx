09596fbf88ae411f683d6ee15cf176a0af5d8f1e
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2024-12-28 12:14:10 +0100

    Add user or buttons depending on if logged in or not

--- client/src/ui/HeaderMegaMenu.tsx
+++ client/src/ui/HeaderMegaMenu.tsx
@@ -1,91 +1,39 @@
 'use client';
-
-import {
-	IconBook,
-	IconChartPie3,
-	IconChevronDown,
-	IconCode,
-	IconCoin,
-	IconFingerprint,
-	IconNotification,
-} from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 import {
 	Anchor,
 	Box,
 	Burger,
 	Button,
-	Center,
-	Collapse,
 	Divider,
 	Drawer,
 	Group,
 	ScrollArea,
-	Text,
-	ThemeIcon,
-	UnstyledButton,
-	useMantineTheme,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from '../css/HeaderMegaMenu.module.css';
 import { UserButton } from './UserButton';
 
-const mockdata = [
-	{
-		icon: IconCode,
-		title: 'Open source',
-		description: 'This Pokémon’s cry is very loud and distracting',
-	},
-	{
-		icon: IconCoin,
-		title: 'Free for everyone',
-		description: 'The fluid of Smeargle’s tail secretions changes',
-	},
-	{
-		icon: IconBook,
-		title: 'Documentation',
-		description: 'Yanma is capable of seeing 360 degrees without',
-	},
-	{
-		icon: IconFingerprint,
-		title: 'Security',
-		description: 'The shell’s rounded shape and the grooves on its.',
-	},
-	{
-		icon: IconChartPie3,
-		title: 'Analytics',
-		description: 'This Pokémon uses its flying ability to quickly chase',
-	},
-	{
-		icon: IconNotification,
-		title: 'Notifications',
-		description: 'Combusken battles with the intensely hot flames it spews',
-	},
-];
-
 export function HeaderMegaMenu() {
 	const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
 		useDisclosure(false);
-	const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-	const theme = useMantineTheme();
 
-	const links = mockdata.map((item) => (
-		<UnstyledButton className={classes.subLink} key={item.title}>
-			<Group wrap="nowrap" align="flex-start">
-				<ThemeIcon size={34} variant="default" radius="md">
-					<item.icon size={22} color={theme.colors.blue[6]} />
-				</ThemeIcon>
-				<div>
-					<Text size="sm" fw={500}>
-						{item.title}
-					</Text>
-					<Text size="xs" c="dimmed">
-						{item.description}
-					</Text>
-				</div>
-			</Group>
-		</UnstyledButton>
-	));
+	const [token, setToken] = useState('');
+	const [userLogged, setUserLogged] = useState(false);
+
+	useEffect(() => {
+		const jwtToken = Cookies.get('jwt');
+		if (jwtToken) {
+			setToken(jwtToken);
+			setUserLogged(true);
+			console.log('User logged in');
+		} else {
+			setUserLogged(false);
+			console.log('User not logged in');
+		}
+	}, []);
 
 	return (
 		<Box pb={120}>
@@ -106,13 +54,17 @@ export function HeaderMegaMenu() {
 					</Group>
 
 					<Group visibleFrom="sm">
-						<Anchor href="/login">
-							<Button variant="default">Log in</Button>
-						</Anchor>
-						<Anchor href="/register">
-							<Button>Sign up</Button>
-						</Anchor>
-						{/* <UserButton /> */}
+						{userLogged && <UserButton />}
+						{!userLogged && (
+							<>
+								<Anchor href="/login">
+									<Button variant="default">Log in</Button>
+								</Anchor>
+								<Anchor href="/register">
+									<Button>Sign up</Button>
+								</Anchor>
+							</>
+						)}
 					</Group>
 
 					<Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
@@ -133,15 +85,6 @@ export function HeaderMegaMenu() {
 					<a href="#" className={classes.link}>
 						Home
 					</a>
-					<UnstyledButton className={classes.link} onClick={toggleLinks}>
-						<Center inline>
-							<Box component="span" mr={5}>
-								Features
-							</Box>
-							<IconChevronDown size={16} color={theme.colors.blue[6]} />
-						</Center>
-					</UnstyledButton>
-					<Collapse in={linksOpened}>{links}</Collapse>
 					<a href="#" className={classes.link}>
 						Learn
 					</a>
@@ -152,8 +95,17 @@ export function HeaderMegaMenu() {
 					<Divider my="sm" />
 
 					<Group justify="center" grow pb="xl" px="md">
-						<Button variant="default">Log in</Button>
-						<Button>Sign up</Button>
+						{userLogged && <UserButton />}
+						{!userLogged && (
+							<>
+								<Anchor href="/login">
+									<Button variant="default">Log in</Button>
+								</Anchor>
+								<Anchor href="/register">
+									<Button>Sign up</Button>
+								</Anchor>
+							</>
+						)}
 					</Group>
 				</ScrollArea>
 			</Drawer>
