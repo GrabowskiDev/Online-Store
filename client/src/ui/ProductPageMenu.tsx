7d09697700030d11cf2e37ccdb33dfd6f61cde56
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2025-01-11 16:03:24 +0100

    add some styles

--- client/src/ui/ProductPageMenu.tsx
+++ client/src/ui/ProductPageMenu.tsx
@@ -9,6 +9,7 @@ import {
 	Group,
 	Stack,
 	NumberInput,
+	Center,
 } from '@mantine/core';
 import { useCounter } from '@mantine/hooks';
 import {
@@ -38,52 +39,58 @@ export default function ProductPageMenu({ product }: ProductPageMenuProps) {
 	return (
 		<Stack
 			justify="space-between"
-			style={{
-				width: '90%',
-				height: '80%',
-			}}>
-			<Box>
-				<Paper shadow="xs" p="md">
-					<Title order={1} c="#212427" pb="md">
-						{product.title}
-					</Title>
-					<Title order={2} c="#212427" mb="xl" pb="md">
-						{product.price}$
-					</Title>
-					<Box style={{ height: '200px', overflowY: 'auto' }}>
-						{showDescription && (
-							<Text c="#212427" size="l">
-								{product.description}
-							</Text>
-						)}
-						<Button
-							onClick={() => setShowDescription(!showDescription)}
-							variant="subtle"
-							mt={showDescription ? 'xs' : 0}
-							pl={0}
-							pr={0}>
-							{showDescription ? 'Hide description' : 'Show description'}
-						</Button>
-					</Box>
-				</Paper>
-			</Box>
-			<Box p="md"></Box>
-			<Box p="md">
-				<Group>
-					<ActionIcon variant="default" size="xl" radius="md" onClick={decrement}>
-						<IconMinus color="var(--mantine-color-gray-text)" />
-					</ActionIcon>
-					<NumberInput
-						size="md"
-						defaultValue={0}
-						value={value}
-						min={0}
-						max={10}
-						readOnly
-						hideControls></NumberInput>
-					<ActionIcon variant="default" size="xl" radius="md" onClick={increment}>
-						<IconPlus color="var(--mantine-color-gray-text)" />
-					</ActionIcon>
+			// style={
+			// 	{
+			// 		// width: '90%',
+			// 		// height: '80%',
+			// 	}
+		>
+			<Paper shadow="xs" p="md" ml="sm" mr="sm" mb={'2rem'}>
+				<Title order={1} c="#212427" pb="md">
+					{product.title}
+				</Title>
+				<Title order={2} c="#212427" mb="xl" pb="md">
+					{product.price}$
+				</Title>
+				<Box style={{ height: '200px', overflowY: 'auto' }}>
+					{showDescription && (
+						<Text c="#212427" size="l">
+							{product.description}
+						</Text>
+					)}
+					<Button
+						onClick={() => setShowDescription(!showDescription)}
+						variant="subtle"
+						mt={showDescription ? 'xs' : 0}
+						pl={0}
+						pr={0}>
+						{showDescription ? 'Hide description' : 'Show description'}
+					</Button>
+				</Box>
+			</Paper>
+			<Box mt="md" ml="sm" mr="sm" mb="md">
+				<Group gap={'0px'} justify="stretch">
+					<Group>
+						<ActionIcon variant="default" size="xl" radius="md" onClick={decrement}>
+							<IconMinus color="var(--mantine-color-gray-text)" />
+						</ActionIcon>
+						<NumberInput
+							size="md"
+							defaultValue={0}
+							value={value}
+							min={0}
+							max={10}
+							readOnly
+							hideControls></NumberInput>
+						<ActionIcon variant="default" size="xl" radius="md" onClick={increment}>
+							<IconPlus color="var(--mantine-color-gray-text)" />
+						</ActionIcon>
+					</Group>
+					<Center style={{ flexGrow: '1' }}>
+						<Text c="#0A0" size="xl">
+							In Stock!
+						</Text>
+					</Center>
 				</Group>
 				<Button
 					justify="center"
@@ -93,7 +100,7 @@ export default function ProductPageMenu({ product }: ProductPageMenuProps) {
 					leftSection={<span />}
 					variant="default"
 					mt="xl">
-					Add to cart
+					ADD TO CART
 				</Button>
 				<Button
 					justify="center"
@@ -103,7 +110,7 @@ export default function ProductPageMenu({ product }: ProductPageMenuProps) {
 					leftSection={<span />}
 					variant="filled"
 					mt="md">
-					Buy now
+					BUY NOW
 				</Button>
 			</Box>
 		</Stack>
